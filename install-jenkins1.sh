1e3779564699a9a3d710ba05fee83b6f3c391b58
UtamaDalington <enter email>
2025-10-08 20:05:28 +0100

    Updated some code

---
+++ install-jenkins1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Update system
+apt-get update -y
+apt-get upgrade -y
+
+# Install Java 11 (LTS)
+sudo apt-get install -y openjdk-11-jdk java-comm
+
+# Install Apache Maven
+apt-get install -y maven 
+
+# Add Jenkins repo key
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+# Add Jenkins repo
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/ | tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Update apt and install Jenkins
+apt-get update -y
+apt-get install -y jenkins
+
+# Enable and start Jenkins
+systemctl enable jenkins
+systemctl start jenkins
